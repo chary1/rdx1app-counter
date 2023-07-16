e91acd9807288067636ecf047f3df4f765e429a0
chary1 <httpchary@gmail.com>
2023-07-17 02:36:00 +0530

    delete todolist

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 function Todolist(props) {
-  console.log(props);
+  //console.log(props);
   return (
     <div className="border-view">
       <h2>Add Todo</h2>
