06bc8d2d939e5d83c8406ec471f0db535cb339ca
chary1 <httpchary@gmail.com>
2023-08-15 05:49:42 +0530

    commited for object in couter

--- src/stores/reducers/todolist.object.reducer.js
+++ src/stores/reducers/todolist.object.reducer.js
@@ -1,23 +1,68 @@
+import { v4 as uuid } from "uuid";
+
 const initialState = {
   todos: [
-    { taskName: "sss", status: true },
-    { taskName: "ttt", status: false },
+    {
+      name: "swimming",
+      status: true,
+      id: uuid(),
+    },
+    {
+      name: "gym",
+      status: false,
+      id: uuid(),
+    },
+    {
+      name: "read",
+      status: true,
+      id: uuid(),
+    },
+    {
+      name: "sleep",
+      status: false,
+      id: uuid(),
+    },
   ],
 };
 
-const todoListObjectReducer = (state = initialState, action) => {
+const todoListReducer = (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
-    case "ADDTODO_TASK":
-      return { ...state, todos: [...state.todos, { ...payload }] };
-    case "REMOVETODO_TASK":
+    case "ADD_TODO":
+      return { ...state, todos: [...state.todos, payload] };
+    case "DELETE_TODO":
       var temp = [...state.todos];
-      const arr = temp.filter(function (el) {
-        return el.taskName !== payload.taskName;
+      //   var removeIndex = temp.findIndex((todo) => todo.id === payload.id);
+      //   console.log(removeIndex);
+      //   if (temp.indexOf(-1)) {
+      //     temp.splice(removeIndex, 1);
+      //   }
+      //   return { ...state, todos: [...temp] };
+
+      const temp_arry = temp.filter(function (el) {
+        if (el.id !== payload.id) {
+          return true;
+        } else {
+          return false;
+        }
       });
-      return { ...state, todos: [...arr] };
+      return { ...state, todos: [...temp_arry] };
+
+    case "TODO_STATUS":
+      var temp1 = [...state.todos];
+      var newArray = temp1.map((todo) => {
+        if (todo.id === payload.id) {
+          todo.status = !todo.status;
+          return todo;
+        } else {
+          return todo;
+        }
+      });
+      console.log(newArray);
+
+      return { ...state, todos: [...newArray] };
     default:
       return state;
   }
 };
-export default todoListObjectReducer;
+export default todoListReducer;
